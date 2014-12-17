06edf6b53e7800e3376a0e8874b14d862616684b
Minzhang Wei <minzhang@dishbranch-lm.peking.corp.yahoo.com>
2014-12-17 20:14:31 +0800

    code base

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -48,6 +48,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 5
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 0
 
+// Baidu-Maps-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Baidu_Maps_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Baidu_Maps_iOS_SDK 2
+#define COCOAPODS_VERSION_MINOR_Baidu_Maps_iOS_SDK 5
+#define COCOAPODS_VERSION_PATCH_Baidu_Maps_iOS_SDK 0
+
 // KDCycleBannerView
 #define COCOAPODS_POD_AVAILABLE_KDCycleBannerView
 #define COCOAPODS_VERSION_MAJOR_KDCycleBannerView 1
